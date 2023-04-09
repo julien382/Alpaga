6a9689e07634e7d5dd48f37c4bf20418e3d9eab2
julien382 <julhermain@gmail.com>
2023-04-09 23:20:47 +0200

    header logo alpaga

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import './Header.css'
 
 import { Link } from "react-router-dom";
-import logo from "../../assets/mainAlpaga.png";
+import logoAlpaga from "../../assets/logoAlpaga.png";
 
 const Header = () => {
 
 
     return (
         <header className='header'>
-            <Link to="/"><img className='logo' src={logo} alt="logo Wealth Health" /></Link>
+            <Link to="/" className='logo'>
+                <img className='logoAlpaga' src={logoAlpaga} alt="logoAlpaga" />
+                <p className='logotexte font color2'>Alpaga</p>
+            </Link>
             <div className='containerNav'>
                 <nav>
                     <Link className='link' to="/">Accueil</Link>
--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -1,5 +1,5 @@
 import './Accueil.css'
-import logo from "../../assets/mainAlpaga.png";
+import alpagaMain from "../../assets/mainAlpaga.png";
 
 const Accueil = () => {
 
@@ -7,10 +7,10 @@ const Accueil = () => {
     return (
         <div className='accueil'>
             <div className='part1Accueil'>
-                <img className='alpagaMain' src={logo} alt="logo Wealth Health" />
+                <img className='alpagaMain' src={alpagaMain} alt="alpagaMain" />
                 <div className='partTexteAccueil'>
                     <h1 className='titleAccueil font color1'>Alpaga</h1>
-                    <p className='paragrapheTitleAccueil'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, <span className='font color1'>des guanacos et des vigognes.</span></p>
+                    <p className='paragrapheTitleAccueil'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, <span className='font color2'>des guanacos et des vigognes.</span></p>
                 </div>
             </div>
             <div className='part2Accueil'>
