b1235b57709543ca3923a5ceb075f72720a85f8d
julien382 <julhermain@gmail.com>
2023-04-09 19:39:34 +0200

    css page error

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import '../styles/reset.css';
+import '../styles/index.css';
+
+import Header from "./Header/Header";
+import Error from "../pages/Error/Error";
+
+function App() {
+
+  return (
+    <div id="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Error />}></Route>
+        <Route path="/*" element={<Error />}></Route>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import './Header.css'
+
+import { Link } from "react-router-dom";
+import logo from "../../assets/mainAlpaga.png";
+
+const Header = () => {
+
+
+    return (
+        <header className='header'>
+            <Link to="/"><img className='logo' src={logo} alt="logo Wealth Health" /></Link>
+            <div className='containerNav'>
+                <h1 className='nameSite'>HRnet</h1>
+                <nav>
+                    <Link className='link' to="/">Create employee</Link>
+                    <Link className='link' to="/employees">Current employees</Link>
+                </nav>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
