5ee7c656ac6b1c5eb93a3a2d0ea24c53f590d73e
julien382 <julhermain@gmail.com>
2023-04-10 19:11:45 +0200

    change texte alpagaMain

--- src/pages/Alpaga/Alpaga.jsx
+++ src/pages/Alpaga/Alpaga.jsx
@@ -11,7 +11,7 @@ const Alpaga = () => {
                 <img className='alpagaMain' src={alpagaMain} alt="alpagaMain" />
                 <div className='partTexteAccueil'>
                     <h1 className='titleAccueil font color1'>Alpaga</h1>
-                    <p className='paragrapheTitleAccueil'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, <span className='font color2'>des guanacos et des vigognes.</span></p>
+                    <p className='paragrapheTitleAccueil'> Les alpagas sont des animaux domestiques originaires d'Amérique du Sud, ils sont proches cousins des lamas, <span className='font color2'>des guanacos et des vigognes.</span></p>
                 </div>
             </div>
             <div className='part2_Accueil'>
