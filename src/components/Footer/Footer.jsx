af8998b61c893d4e12ad714776792e127e564974
julien382 <julhermain@gmail.com>
2023-04-10 16:41:50 +0200

    add link contact

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <footer className='footer'>
             <Link to="/mentionsLegales" className='mentionsLegales'>Mentions légales</Link>
             <Link to="/copyright" className='copyright'>2023 © Alpaga</Link>
-            <Link to="/contact" className='contact'>Contact</Link>
+            <Link to="https://www.linkedin.com/in/julien-hermain/" className='contact'>Contact</Link>
         </footer>
     )
 }
