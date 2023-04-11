9b8d7ebdc3dfcbb1062b459f5154e454cf18b5ad
julien382 <julhermain@gmail.com>
2023-04-12 00:45:48 +0200

    gameChoice in pageLama

--- src/pages/Lama/Lama.jsx
+++ src/pages/Lama/Lama.jsx
@@ -2,8 +2,58 @@ import './Lama.css'
 import alpagaMain from "../../assets/mainAlpaga.png";
 import alpagaBrown from "../../assets/alpagaBrown.png";
 
+import truemainAlpaga from "../../assets/mainAlpaga.png";
+import truealpaca from "../../assets/alpaca-5564884_1280_preview_rev_1.png";
+import truealpaga404 from "../../assets/alpaga404.png";
+import falsemainAlpaga from "../../assets/alpagaAlimentation.png";
+import falsealpaca from "../../assets/logoAlpaga.png";
+import falsealpaga404 from "../../assets/alpagaBrown.png";
+import { useEffect, useState } from 'react';
+
+const allImagesTrue = [
+  { src: truemainAlpaga, isCorrect: true},
+  { src: truealpaca, isCorrect: true},
+  { src: truealpaga404, isCorrect: true},
+];
+
+const allImagesFalse = [
+  { src: falsemainAlpaga, isCorrect: false},
+  { src: falsealpaca, isCorrect: false},
+  { src: falsealpaga404, isCorrect: false},
+];
+
 const Lama = () => {
 
+    const [scoreWin, setScoreWin] = useState(0);
+    const [scoreLoose, setScoreLoose] = useState(0);
+    const [currentImages, setCurrentImages] = useState([]);
+
+    const handleImageClick = (image) => {
+        if (image.isCorrect) {
+        setScoreWin(scoreWin + 1);
+        const newImages = generateImages();
+        setCurrentImages(newImages);
+        }
+        else{
+        setScoreLoose(scoreLoose + 1);
+        const newImages = generateImages();
+        setCurrentImages(newImages);
+        }
+    };
+
+    // Générer un tableau de deux images aléatoires à partir du tableau de toutes les images possibles
+    const generateImages = () => {
+        const correctImage = allImagesTrue[Math.floor(Math.random() * allImagesTrue.length)];
+        const incorrectImage = allImagesFalse[Math.floor(Math.random() * allImagesFalse.length)];
+        const selectedImages = [correctImage, incorrectImage].sort(() => 0.5 - Math.random());
+        return selectedImages;
+    };
+
+    // Initialiser le jeu avec un premier tableau d'images
+    useEffect(() => {
+        const newImages = generateImages();
+        setCurrentImages(newImages);
+    }, []);
 
     return (
         <div className='accueil'>
@@ -22,12 +72,27 @@ const Lama = () => {
                 </div>
                 <img className='part2_alpagaImage' src={alpagaBrown} alt="alpagaBrown" />
             </div>
-            <div className='part3_Accueil'>
-                <p className='part3_Paragraphe1'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, des guanacos et des vigognes.</p>
-                <p className='part3_Paragraphe2'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, des guanacos et des vigognes.</p>
-                <p className='part3_Paragraphe3'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, des guanacos et des vigognes.</p>
+            <div className='gameChoice'>
+                <div className='containerScoreBoard'>
+                    <p className='scoreBoardWin font'>V: {scoreWin}</p>
+                    <p className='titleGame font'>Trouve l'alpaga:</p>
+                    <p className='scoreBoardLoose font'>X: {scoreLoose}</p>
+                </div>
+                <div className='picturesGame'>
+                    {currentImages.map((image) => (
+                    <img
+                        className='pictureGame'
+                        key={image.src}
+                        src={image.src}
+                        alt={image.src}
+                        onClick={() => handleImageClick(image)}
+                    />
+                    ))}
+                    {currentImages.map((image) => (
+                    <p>{image.isCorrect.toString()}</p>
+                    ))}
+                </div>
             </div>
-            
         </div>
     )
 }
--- src/pages/Production/Production.jsx
+++ src/pages/Production/Production.jsx
@@ -1,62 +1,10 @@
 import './Production.css';
-import React, { useEffect, useState } from 'react';
-import truemainAlpaga from "../../assets/mainAlpaga.png";
-import truealpaca from "../../assets/alpaca-5564884_1280_preview_rev_1.png";
-import truealpaga404 from "../../assets/alpaga404.png";
-import falsemainAlpaga from "../../assets/alpagaAlimentation.png";
-import falsealpaca from "../../assets/logoAlpaga.png";
-import falsealpaga404 from "../../assets/alpagaBrown.png";
+import React, { useState } from 'react';
 
 import notTondu from "../../assets/notTondu.png";
 import tondu from "../../assets/tondu.png";
 
-const allImagesTrue = [
-  { src: truemainAlpaga, isCorrect: true},
-  { src: truealpaca, isCorrect: true},
-  { src: truealpaga404, isCorrect: true},
-];
-
-const allImagesFalse = [
-  { src: falsemainAlpaga, isCorrect: false},
-  { src: falsealpaca, isCorrect: false},
-  { src: falsealpaga404, isCorrect: false},
-];
-
 const Production = () => {
-
-  const [scoreWin, setScoreWin] = useState(0);
-  const [scoreLoose, setScoreLoose] = useState(0);
-  const [currentImages, setCurrentImages] = useState([]);
-
-  const handleImageClick = (image) => {
-    if (image.isCorrect) {
-      setScoreWin(scoreWin + 1);
-      const newImages = generateImages();
-      setCurrentImages(newImages);
-    }
-    else{
-      setScoreLoose(scoreLoose + 1);
-      const newImages = generateImages();
-      setCurrentImages(newImages);
-    }
-  };
-
-  // Générer un tableau de deux images aléatoires à partir du tableau de toutes les images possibles
-  const generateImages = () => {
-    const correctImage = allImagesTrue[Math.floor(Math.random() * allImagesTrue.length)];
-    const incorrectImage = allImagesFalse[Math.floor(Math.random() * allImagesFalse.length)];
-    const selectedImages = [correctImage, incorrectImage].sort(() => 0.5 - Math.random());
-    return selectedImages;
-  };
-
-  // Initialiser le jeu avec un premier tableau d'images
-  useEffect(() => {
-    const newImages = generateImages();
-    setCurrentImages(newImages);
-  }, []);
-
-
-
 
   const [isFaded, setIsFaded] = useState(false);
   const [currentImage, setCurrentImage] = useState(notTondu);
@@ -75,37 +23,13 @@ const Production = () => {
   
 
   return (
-    <div className='gameContainer'>
-      <div className='containerScoreBoard'>
-        <p className='scoreBoardWin font'>V: {scoreWin}</p>
-        <p className='titleGame font'>Trouve l'alpaga:</p>
-        <p className='scoreBoardLoose font'>X: {scoreLoose}</p>
-      </div>
-      <div className='picturesGame'>
-        {currentImages.map((image) => (
-          <img
-            className='pictureGame'
-            key={image.src}
-            src={image.src}
-            alt={image.src}
-            onClick={() => handleImageClick(image)}
-          />
-        ))}
-        {currentImages.map((image) => (
-          <p>{image.isCorrect.toString()}</p>
-        ))}
-      </div>
-
-
-      <div className="image-container">
-        <img
-          src={currentImage}
-          alt="Image"
-          className={`image ${isFaded ? 'fade-out' : 'fade-in'}`}
-          onClick={handleClick}
-        />
-      </div>
-
+    <div className="image-container">
+      <img
+        src={currentImage}
+        alt={currentImage}
+        className={`image ${isFaded ? 'fade-out' : 'fade-in'}`}
+        onClick={handleClick}
+      />
     </div>
   );
 }
