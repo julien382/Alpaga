4fb2c7e10a3416406e6eea4c45ba60fac065ec1d
julien382 <julhermain@gmail.com>
2023-04-12 21:29:56 +0200

    edit imageTracker in component

---
+++ src/components/ImageTracker/ImageTracker.jsx
@@ -0,0 +1,35 @@
+import './ImageTracker.css'
+import alpagaMain from "../../assets/alpaca-4366367_1280.jpg";
+
+import React, { useState } from 'react';
+
+const ImageTracker = () => {
+    
+    const [transform, setTransform] = useState({
+        rotateX: 0,
+        rotateY: 0
+      });
+    
+      const handleMouseMove = (event) => {
+        const { left, top, width, height } = event.currentTarget.getBoundingClientRect();
+        const x = (event.clientX - left) / width;
+        const y = (event.clientY - top) / height;
+        const newX = (y - 0.5) * 20;
+        const newY = (x - 0.5) * 20;
+        setTransform({ rotateX: newX, rotateY: newY });
+      };
+
+      
+    return (
+        <div className='imageTracker'>
+            <div className="container" onMouseMove={handleMouseMove}>
+                <div className="image" style={{ transform: `perspective(1000px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg)` }}>
+                    <img src={alpagaMain} alt={alpagaMain} />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ImageTracker
--- src/pages/Elevage/Elevage.jsx
+++ src/pages/Elevage/Elevage.jsx
@@ -1,30 +1,16 @@
 import './Elevage.css'
 import alpagaMain from "../../assets/alpaca-4366367_1280.jpg";
+import alpagaLogo from "../../assets/logoAlpaga.png";
 
-import React, { useState } from 'react';
+import ImageTracker from '../../components/ImageTracker/ImageTracker';
 
 const Elevage = () => {
     
-    const [transform, setTransform] = useState({
-        rotateX: 0,
-        rotateY: 0
-      });
-    
-      const handleMouseMove = (event) => {
-        const { left, top, width, height } = event.currentTarget.getBoundingClientRect();
-        const x = (event.clientX - left) / width;
-        const y = (event.clientY - top) / height;
-        const newX = (y - 0.5) * 20;
-        const newY = (x - 0.5) * 20;
-        setTransform({ rotateX: newX, rotateY: newY });
-      };
+
     return (
         <div className='elevage'>
-            <div className="container" onMouseMove={handleMouseMove}>
-                <div className="image" style={{ transform: `perspective(1000px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg)` }}>
-                    <img src={alpagaMain} alt={alpagaMain} />
-                </div>
-            </div>
+            <ImageTracker image={alpagaMain} />
+            <ImageTracker image={alpagaLogo} />
 
         </div>
     )
