36933a3902e1473665b58b9726a8d14cbf2e4686
julien382 <julhermain@gmail.com>
2023-04-12 19:55:23 +0200

    create imageTrackerHover

--- src/pages/Elevage/Elevage.jsx
+++ src/pages/Elevage/Elevage.jsx
@@ -1,10 +1,30 @@
 import './Elevage.css'
+import alpagaMain from "../../assets/alpaca-4366367_1280.jpg";
+
+import React, { useState } from 'react';
 
 const Elevage = () => {
-
-
+    
+    const [transform, setTransform] = useState({
+        rotateX: 0,
+        rotateY: 0
+      });
+    
+      const handleMouseMove = (event) => {
+        const { left, top, width, height } = event.currentTarget.getBoundingClientRect();
+        const x = (event.clientX - left) / width;
+        const y = (event.clientY - top) / height;
+        const newX = (y - 0.5) * 20;
+        const newY = (x - 0.5) * 20;
+        setTransform({ rotateX: newX, rotateY: newY });
+      };
     return (
         <div className='elevage'>
+            <div className="container" onMouseMove={handleMouseMove}>
+                <div className="image" style={{ transform: `perspective(1000px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg)` }}>
+                    <img src={alpagaMain} alt={alpagaMain} />
+                </div>
+            </div>
 
         </div>
     )
