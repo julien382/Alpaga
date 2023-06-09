54b4209f5334cc900151513d6528f957b7f8e8fa
julien382 <julhermain@gmail.com>
2023-06-09 15:59:03 +0200

    add contenu page alpaga

--- src/pages/Alpaga/Alpaga.jsx
+++ src/pages/Alpaga/Alpaga.jsx
@@ -16,16 +16,20 @@ const Alpaga = () => {
             </div>
             <div className='part2_Accueil'>
                 <div className='part2_textContainer'>
-                    <p className='part2_Paragraphe1'>Les alpagas ont été domestiqués il y a environ 6 000 ans par les peuples indigènes des Andes.</p>
-                    <p className='part2_Paragraphe2'>Les alpagas ont été domestiqués il y a environ 6 000 ans par les peuples indigènes des Andes. </p>
-                    <p className='part2_Paragraphe3'>Il y a beaucoup d'autres aspects intéressants à raconter sur les alpagas, ceci n'est qu'un exemple. Il existe de nombreux aspects intéressants à raconter sur l'histoire, les caractéristiques, les avantages écologiques, les différentes races, la domestication, l'élevage, l'utilisation de leur laine, leur viande, leur lait et bien plus encore.</p>
+                    <p className='part2_Paragraphe1'>Les alpagas ont une silhouette élégante et gracile. Ils sont plus petits que les lamas, avec une hauteur au garrot allant de 81 à 99 cm environ. Leur pelage est dense, soyeux et disponible dans une large gamme de couleurs, notamment le blanc, le noir, le brun, le gris et le beige.</p>
+                    <p className='part2_Paragraphe2'>Les alpagas sont des animaux dociles et curieux. Ils sont généralement amicaux et s'adaptent facilement à l'élevage.</p>
+                    <p className='part2_Paragraphe3'>Les alpagas sont considérés comme une espèce domestique, mais certaines populations d'alpagas sauvages existent toujours dans les régions reculées des Andes. Des efforts de conservation sont déployés pour préserver la diversité génétique de ces populations sauvages.</p>
                 </div>
-                <img className='part2_alpagaImage' src={alpagaBrown} alt="alpagaBrown" />
+                <div className='part2_imageTextContainer'>
+                    <p className='part2_Paragraphe4'>La durée de vie moyenne d'un alpaga est d'environ 15 à 20 ans</p>
+                    <img className='part2_alpagaImage' src={alpagaBrown} alt="alpagaBrown" />
+                    <p className='part2_Paragraphe5'> En général, les alpagas adultes pèsent entre 55 et 84 kg, avec une moyenne d'environ 65 kg pour les femelles et 75 kg pour les mâles.</p>
+                </div>
             </div>
             <div className='part3_Accueil'>
-                <p className='part3_Paragraphe1'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, des guanacos et des vigognes.</p>
-                <p className='part3_Paragraphe2'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, des guanacos et des vigognes.</p>
-                <p className='part3_Paragraphe3'>Les alpagas sont des animaux domestiques originaires des Andes du Sud-Amérique, ils sont proches cousins des lamas, des guanacos et des vigognes.</p>
+                <img className='part2_alpagaImage' src={alpagaBrown} alt="alpagaBrown" />
+                <p className='part3_Paragraphe1'>Un bébé alpaga est appelé un "cria". Ils allaitent leur mère pendant plusieurs mois, dépendant entièrement du lait maternel pour leur nutrition. Ils commencent généralement à consommer progressivement de la végétation solide, comme l'herbe et le foin, autour de 2 à 3 semaines</p>
+                <img className='part2_alpagaImage' src={alpagaBrown} alt="alpagaBrown" />
             </div>
             
         </div>
