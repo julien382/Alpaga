7c156c4d338a39f6c85cfd8723fa303742e189be
julien382 <julhermain@gmail.com>
2023-06-12 21:44:15 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const isLocalDevelopment = process.env.NODE_ENV === 'development';
+const basename = isLocalDevelopment ? '/' : '/Alpaga';
+
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
+    <BrowserRouter basename={basename}>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
