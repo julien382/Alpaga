b45deb38c7269ce69c5739ab5a51a2f93595bc0d
julien382 <julhermain@gmail.com>
2023-06-13 17:51:28 +0200

    utlisation du component accueilType sur la page race et production

--- src/pages/Production/Production.jsx
+++ src/pages/Production/Production.jsx
@@ -10,6 +10,7 @@ import tondu from "../../assets/tondu.png";
 import laine from "../../assets/laine.jpg";
 import alpagablanc from "../../assets/2alpagablanc.jpg";
 import tonte from "../../assets/tonte.jpg";
+import AccueilType from '../../components/AccueilType/AccueilType';
 
 const Production = () => {
 
@@ -22,40 +23,35 @@ const Production = () => {
 
   return (
     <div className='production'>
-            <div className='part1_Production'>
-                <div className='partTexteProduction'>
-                    <h1 className='titleProduction font color1'>Prodution</h1>
-                    <p className='paragrapheTitleProduction'>Les alpagas sont élevés principalement pour leur laine de grande qualité.</p>
-                </div>
-                <img className='alpagaMainProduction' src={tonteDebout} alt={alpagaMain} />
-            </div>
-            <div className='productionImages'>
-              <ImageTracker image={alpagablanc} />
-              <ImageTracker image={laine} />
-              <ImageTracker image={tonte} />
-            </div>
-            <div className='part2_production'>
-                <p className='part2_production_Paragraphe1'>Les alpagas produisent une laine de haute qualité et recherchée. La laine d'alpaga est appréciée pour sa douceur, sa légèreté et sa chaleur. Elle peut être transformée en fils, en tissus et en produits finis tels que des vêtements, des accessoires et des articles de décoration.</p>
-                <p className='part2_production_Paragraphe2'> La production de laine d'alpaga implique généralement la tonte régulière des animaux pour récolter leur toison.</p>
-            </div>
-            <div className='tonduFondu'>
-              <h1 className='titleTonduFondu font color1'>Tond l'alpaga :</h1>
-              <div className="image-container">
-                <img
-                  className='imagetonduFondu'
-                  src={imageSrc}
-                  alt={imageSrc}
-                  onClick={handleClick}
-                  style={{
-                    transition: 'opacity 0.5s ease-in-out',
-                    opacity: 1,
-                    maxWidth: '100%',
-                  }}
-                />
-              </div>
-            </div>
-            
+      <AccueilType image={tonteDebout} title="Prodution" texte="Les alpagas sont élevés principalement pour leur laine de grande qualité."> 
+      </AccueilType>
+      <div className='productionImages'>
+        <ImageTracker image={alpagablanc} />
+        <ImageTracker image={laine} />
+        <ImageTracker image={tonte} />
+      </div>
+      <div className='part2_production'>
+          <p className='part2_production_Paragraphe1'>Les alpagas produisent une laine de haute qualité et recherchée. La laine d'alpaga est appréciée pour sa douceur, sa légèreté et sa chaleur. Elle peut être transformée en fils, en tissus et en produits finis tels que des vêtements, des accessoires et des articles de décoration.</p>
+          <p className='part2_production_Paragraphe2'> La production de laine d'alpaga implique généralement la tonte régulière des animaux pour récolter leur toison.</p>
+      </div>
+      <div className='tonduFondu'>
+        <h1 className='titleTonduFondu font color1'>Tond l'alpaga :</h1>
+        <div className="image-container">
+          <img
+            className='imagetonduFondu'
+            src={imageSrc}
+            alt={imageSrc}
+            onClick={handleClick}
+            style={{
+              transition: 'opacity 0.5s ease-in-out',
+              opacity: 1,
+              maxWidth: '100%',
+            }}
+          />
         </div>
+      </div>
+      
+    </div>
   );
 }
 
--- src/pages/Race/Race.jsx
+++ src/pages/Race/Race.jsx
@@ -2,6 +2,7 @@ import './Race.css'
 import alpagaMain from "../../assets/alpagaMountain_cut.png";
 import alpagaSuri from "../../assets/suri_reverse_cut.png";
 import huacaya from "../../assets/huacaya.png";
+import AccueilType from '../../components/AccueilType/AccueilType';
 
 
 const Race = () => {
@@ -9,13 +10,10 @@ const Race = () => {
 
     return (
         <div className='race'>
-            <div className='part1_race'>
-                <div className='partTexteRace'>
-                    <h1 className='titleRace font color1'>Race</h1>
-                    <p className='paragrapheTitleRace'>On peut distinguer deux grandes catégories : <span className='font color2'>les Huacayas et les Suris. </span> </p>
-                </div>
-                <img className='alpagaMainRace' src={alpagaMain} alt="alpagaMain" />
-            </div>
+            <AccueilType image={alpagaMain} title="Race" texte={
+                <span>On peut distinguer deux grandes catégories :<span className="font color2"> les Huacayas et les Suris.</span>
+                </span>}> 
+            </AccueilType>
             <div className='part2_race'>
                 <div className='containerHuacanyas'>
                     <p className='part2_race_Paragraphe1'><span className='font color1'>Les Huacayas </span> ont une laine dense, douce et bouclée, qui leur donne un aspect moelleux et rond. Leur laine est très prisée pour les vêtements et les couvertures car elle est chaude et douillette. Les Huacayas sont également connus pour leur conformation compacte et leur apparence robuste, ce qui les rend facilement reconnaissables. Ils ont une taille moyenne, pesant entre 55 et 79 kg pour les mâles et entre 48 et 64 kg pour les femelles.</p>
