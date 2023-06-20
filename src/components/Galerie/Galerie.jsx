e3f2be3fe5fd3f1923b447fc195dd4016db6d372
julien382 <julhermain@gmail.com>
2023-06-20 15:37:16 +0200

    add data in galerie

--- src/components/Galerie/Galerie.jsx
+++ src/components/Galerie/Galerie.jsx
@@ -1,58 +1,66 @@
 import './Galerie.scss'
-import alpagaMain from "../../assets/alpagaMountain_cut.png";
-import alpagablanc from "../../assets/2alpagablanc.jpg";
-import tonte from "../../assets/tonte.jpg";
-
+import damian from "../../assets/damian-barczak-XIDi_OUMU-I-unsplash.jpg";
+import josiah from "../../assets/josiah-farrow-mYxtYpSJvbw-unsplash.jpg";
+import pexels from "../../assets/pexels-ali-atakan-açıkbaş-10595504.jpg";
+import istockphoto from "../../assets/istockphoto-640829652-612x612.jpg";
+import istockphoto2 from "../../assets/istockphoto-160558081-612x612.jpg";
+import pexels2 from "../../assets/pexels-ali-atakan-açıkbaş-10595518.jpg";
+import alpaca from "../../assets/alpaca-gb3f7779e3_1920.jpg";
+import pexels3 from "../../assets/pexels-paige-thompson-13990654.jpg";
+import alpaca2 from "../../assets/alpaca-4366367_1280.jpg";
+import arne from "../../assets/arne-verbist-JfR2z3v7w0M-unsplash.jpg";
+import crias from "../../assets/crias.jpg";
+import pexels4 from "../../assets/pexels-ali-atakan-açıkbaş-10595510.jpg";
 
 const Galerie = () => {
 
   const images = [
     {
-      src: alpagaMain,
+      src: damian,
       alt: 'Image 1',
     },
     {
-      src: alpagablanc,
+      src: josiah,
       alt: 'Image 2',
     },
     {
-      src: tonte,
+      src: pexels,
       alt: 'Image 3',
     },
     {
-      src: alpagaMain,
+      src: istockphoto,
       alt: 'Image 4',
     },
     {
-      src: alpagablanc,
+      src: istockphoto2,
       alt: 'Image 5',
     },
     {
-      src: tonte,
+      src: pexels2,
       alt: 'Image 6',
     },
     {
-      src: alpagaMain,
+      src: alpaca,
       alt: 'Image 7',
     },
     {
-      src: alpagablanc,
+      src: pexels3,
       alt: 'Image 8',
     },
     {
-      src: tonte,
+      src: alpaca2,
       alt: 'Image 9',
     },
     {
-      src: alpagaMain,
+      src: arne,
       alt: 'Image 10',
     },
     {
-      src: alpagablanc,
+      src: crias,
       alt: 'Image 11',
     },
     {
-      src: tonte,
+      src: pexels4,
       alt: 'Image 12',
     },
   ];
