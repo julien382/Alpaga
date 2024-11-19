66c60895bbb9420380223cadd6777f4500fcc16c
julien382 <julhermain@gmail.com>
2024-11-19 20:01:45 +0100

    add bold

--- src/pages/Alpaga/Alpaga.jsx
+++ src/pages/Alpaga/Alpaga.jsx
@@ -24,23 +24,23 @@ const Alpaga = () => {
             
             <div className='part2_Accueil'>
                 <div className='part2_mainText'>
-                    <p>Les alpagas sont considérés comme une espèce domestique, mais certaines populations d'alpagas sauvages existent toujours dans les régions reculées des Andes. Des efforts de conservation sont déployés pour préserver la diversité génétique de ces populations sauvages.</p>
+                    <p>Les alpagas sont considérés comme <strong>une espèce domestique</strong>, mais certaines populations d'alpagas sauvages existent toujours dans les régions reculées des Andes. Des efforts de conservation sont déployés pour préserver la diversité génétique de ces populations sauvages.</p>
                 </div>
                 <div className='part2_containerTextImage'>
                     <div className='part2_imageContainer'>
                         <img className='part2_image' src={alpagaMarron} alt="alpagaMarron" />
                     </div>
                     <div className='part2_paragraphe1'>
-                        <p>Leur pelage est dense, soyeux et disponible dans une large gamme de couleurs, notamment le blanc, le noir, le brun, le gris et le beige.</p>
+                        <p><strong>Leur pelage</strong> est dense, soyeux et disponible dans une large gamme de couleurs, notamment <strong>le blanc, le noir, le brun, le gris et le beige.</strong></p>
                     </div>
-                    <InfoVisuel image={Size} text={<span>Plus petits que les lamas, avec une <strong>hauteur au garrot</strong> allant de 81 à 99 cm environ.</span>}/>
+                    <InfoVisuel image={Size} text={<span>Plus petits que les lamas, avec une <strong>hauteur au garrot</strong> allant de <strong>81 à 99 cm</strong> environ.</span>}/>
 
                 </div>
                 <div className='part2_containerPoidsParagraphe2'>
-                    <InfoVisuel image={Poids} text={"≃ 55 à 84 kg"}/>
-                    <InfoVisuel image={Age} text={"≃ 15 à 20 ans"}/>
+                    <InfoVisuel image={Poids} text={<strong>≃ 55 à 84 kg</strong>}/>
+                    <InfoVisuel image={Age} text={<strong>≃ 15 à 20 ans</strong>}/>
                     <div className='part2_paragraphe2'>
-                        <p>Ce sont des animaux dociles et curieux. Ils sont généralement amicaux et s'adaptent facilement à l'élevage.</p>
+                        <p>Ce sont <strong>des animaux dociles et curieux.</strong> Ils sont généralement <strong>amicaux</strong> et s'adaptent facilement à l'élevage.</p>
                     </div>
 
                 </div>
