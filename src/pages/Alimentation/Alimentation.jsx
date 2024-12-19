c7b7c57bf118b4b5be4fe85193be6caa5ce2aab5
julien382 <julhermain@gmail.com>
2024-12-19 17:34:18 +0100

    change design alimentation

--- src/pages/Alimentation/Alimentation.jsx
+++ src/pages/Alimentation/Alimentation.jsx
@@ -10,18 +10,13 @@ const Alimentation = () => {
             <AccueilType image={alpagaMain} title="Alimentation" texte="Les alpagas sont des animaux herbivores qui ont besoin d'une alimentation équilibrée et nutritive pour maintenir leur santé et leur bien-être."> 
             </AccueilType>
             <div className='part2_alimentation'>
-                <div>
-                    <div className='part2_alimentation_paragraphe1'>
-                        <p>L’herbe est leur aliment principal. Ils adorent brouter dans les prairies</p>
-                    </div>
-                    <div className='part2_alimentation_paragraphe2'>
-                        <p>Le foin est un complément important, surtout en hiver ou lorsque l'herbe est moins abondante. Des compléments minéraux et vitaminés peuvent être nécessaires pour assurer un apport équilibré en nutriments.</p>
-                    </div>
+                <div className='part2_alimentation_paragraphe1'>
+                    <p>L’herbe est leur aliment principal. Ils adorent brouter dans les prairies</p>
+                </div>
+                <div className='part2_alimentation_paragraphe2'>
+                    <p>Le foin est un complément important, surtout en hiver ou lorsque l'herbe est moins abondante. Des compléments minéraux et vitaminés peuvent être nécessaires pour assurer un apport équilibré en nutriments.</p>
+                </div>
 
-                </div>
-                <div className='part2_imageContainer'>
-                    <img className='part2_image' src={alpagaMain} alt="alpagaMarron" />
-                </div>
             </div>
         </div>
     )
