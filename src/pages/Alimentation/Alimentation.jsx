862d1037387b7f88e4f86525c3e3241e0e79734d
julien382 <julhermain@gmail.com>
2024-12-19 17:27:46 +0100

    change margin pagesTypes

--- src/pages/Alimentation/Alimentation.jsx
+++ src/pages/Alimentation/Alimentation.jsx
@@ -10,9 +10,18 @@ const Alimentation = () => {
             <AccueilType image={alpagaMain} title="Alimentation" texte="Les alpagas sont des animaux herbivores qui ont besoin d'une alimentation équilibrée et nutritive pour maintenir leur santé et leur bien-être."> 
             </AccueilType>
             <div className='part2_alimentation'>
-                <p className='part2_alimentation_Paragraphe1'>Le foin ou la paille de qualité supérieure sont la base de leur alimentation, avec une consommation quotidienne d'environ 2 à 3% de leur poids corporel. L'accès à de l'eau propre et fraîche en permanence est également essentiel.</p>
-                <p className='part2_alimentation_Paragraphe2'>En plus du foin, les alpagas peuvent être nourris avec des aliments commerciaux pour alpagas, des suppléments minéraux, des vitamines, des légumes, des fruits et des herbes.</p>
-                <p className='part2_alimentation_Paragraphe3'>Il est important de surveiller attentivement l'état de santé des alpagas et d'adapter leur régime alimentaire en conséquence pour prévenir les problèmes de santé courants.</p>
+                <div>
+                    <div className='part2_alimentation_paragraphe1'>
+                        <p>L’herbe est leur aliment principal. Ils adorent brouter dans les prairies</p>
+                    </div>
+                    <div className='part2_alimentation_paragraphe2'>
+                        <p>Le foin est un complément important, surtout en hiver ou lorsque l'herbe est moins abondante. Des compléments minéraux et vitaminés peuvent être nécessaires pour assurer un apport équilibré en nutriments.</p>
+                    </div>
+
+                </div>
+                <div className='part2_imageContainer'>
+                    <img className='part2_image' src={alpagaMain} alt="alpagaMarron" />
+                </div>
             </div>
         </div>
     )
